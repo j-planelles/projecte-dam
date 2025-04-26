bd287f42cc10d575e3ad371a548a0bf0dd6a66f3
j-planelles <05jordiplanelles@gmail.com>
2025-04-26 15:17:29 +0200

    Android Development Build & switch to Material You themeing

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -7,16 +7,19 @@ import { RestCountdownProvider } from "../store/rest-timer-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 import { useColorScheme } from "react-native";
-import { customDarkTheme, customLightTheme } from "../lib/theme";
 
 const client = new QueryClient();
 
 export default function MainLayout() {
   const colorScheme = useColorScheme();
-  const { theme } = useMaterial3Theme(); // TODO: Implement Material 3 Theme
+  const { theme } = useMaterial3Theme({
+    fallbackSourceColor: "769CDF",
+  });
 
   const paperTheme =
-    colorScheme === "dark" ? { ...customDarkTheme } : { ...customLightTheme };
+    colorScheme === "dark"
+      ? { ...MD3DarkTheme, colors: theme.dark }
+      : { ...MD3LightTheme, colors: theme.light };
 
   return (
     <>
